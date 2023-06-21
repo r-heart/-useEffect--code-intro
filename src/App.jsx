190c97c2982643a9d852231377db3dc42204a24a
r-heart <rachaelkheart@gmail.com>
2023-06-21 10:44:50 -0700

    feat: :sparkles: write first useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import React from "react";
+
 function App() {
+  React.useEffect(() => {
+    console.log("Hello world!");
+  });
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
